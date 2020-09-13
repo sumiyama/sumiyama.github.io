77d7e3fde7ed69979c6b7629fc6c08eb445c79fd
sumiyama <66947811+sumiyama@users.noreply.github.com>
2020-09-13 09:45:55 +0900

    Add files via upload

---
+++ kanicity.js
@@ -0,0 +1,11 @@
+﻿var scrollAnimationElm = document.querySelectorAll('.pop,.pop_4s');
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    var triggerMargin = 80;
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+      scrollAnimationElm[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+window.addEventListener('scroll', scrollAnimationFunc);
---
+++ scrolltop.js
@@ -0,0 +1,16 @@
+﻿let $pagetop = $('.scrolltop,.scrollmenu');
+
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() < 20) {
+        $pagetop.removeClass('isActive');
+    } else {
+        $pagetop.addClass('isActive');
+    }
+});
+$('a[href^="#"]').on('click', function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 550, "swing");
+    return false;
+});
